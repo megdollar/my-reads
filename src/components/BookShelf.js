704f9a36774721c461d30ab4b48af53f465cd38b
Megan Dollar <megdollar@gmail.com>
2017-09-20 20:33:37 -0400

    bookshelf to functional component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class BookShelf extends Component {
-    render(){
-        const { books, onHandleChange } = this.props
-            
+
+ function BookShelf (props) {           
     return(
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+                <h2 className="bookshelf-title">{props.shelfName}</h2>
                 <div className="bookshelf-books">
 
                     <ol className="books-grid">
-                         {books.map((book)=>(
+                         {props.books.map((book)=>(
                             <li key={book.id}>
                         <div className="book">
                             <div className="book-top">
                                 <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                 <div className="book-shelf-changer">
                                     <select value={book.shelf} 
-                                        onChange={(e) => onHandleChange(book,e.target.value)}>
+                                        onChange={(e) => props.onHandleChange(book,e.target.value)}>
                                         <option value="" disabled>Move to...</option>
                                         <option value="currentlyReading">Currently Reading</option>
                                         <option value="wantToRead">Want to Read</option>
@@ -39,7 +37,7 @@ class BookShelf extends Component {
                 </div>
             </div>
         )
-    }
 }
+
 
 export default BookShelf
